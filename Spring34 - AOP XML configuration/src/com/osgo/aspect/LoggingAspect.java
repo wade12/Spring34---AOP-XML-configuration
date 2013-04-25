11c5998e7a86e78337b8dfef11f53b40fe3f55b5
wade <wade@Phenom>
2013-04-25 14:52:04 +0100

    Spring34 - AOP XML configuration.git

---
+++ Spring34 - AOP XML configuration/src/com/osgo/aspect/LoggingAspect.java
@@ -0,0 +1,74 @@
+package com.osgo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LoggingAspect
+{
+	@Before("allCircleMethods()")
+	public void loggingAdvice(JoinPoint joinPoint)
+	{	
+	} // end method loggingAdvice
+	
+	
+	@AfterReturning(pointcut="args(name)", returning="returnString")
+	public void stringArgumentMethod(String name, Object returnString)
+	{
+		System.out.println("A method that takes String arguments has been called.  The value is: " + name);
+		System.out.println("The output value is: " + returnString);
+	} // end method stringArgumentMethod
+	
+	
+	@AfterThrowing(pointcut="args(name)", throwing="exception")
+	public void exceptionAdvice(String name, Exception exception)
+	{
+		System.out.println("An exception has been thrown:" + (exception));
+	} // end method exceptionAdvice
+	
+	
+	// @Around("allGetters()")
+	@Around("@annotation(com.osgo.aspect.Loggable)")
+	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint)
+	{
+		Object returnValue = null;
+		try
+		{
+			System.out.println("Before Advice.");
+			returnValue = proceedingJoinPoint.proceed();					// target method execution
+			System.out.println("After Returning.");
+		}
+		catch (Throwable exception)
+		{
+			System.out.println("After Throwing.");
+		}
+		
+		System.out.println("After Finally.");
+		return returnValue;
+		
+	} // end method myAroundAdvice
+	
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters()
+	{
+	} // end dummy method allGetters
+	
+	
+	// @Pointcut("execution(* com.osgo.service.*Service.*(..))")
+	
+	
+	@Pointcut("within(com.osgo.model.Circle)")						
+	public void allCircleMethods()
+	{
+	} // end dummy method allCircleMethods()
+		
+	
+} // end Class LoggingAspect
